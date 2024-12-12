0272b50aba6ccefb179bf22735ba261d3b794f0b
Tosin0814 <ayanleyetosin@gmail.com>
2024-12-12 16:59:09 -0500

    Create patent search form with start and end range

--- src/components/PatentSearchForm/PatentSearchForm.jsx
+++ src/components/PatentSearchForm/PatentSearchForm.jsx
@@ -5,8 +5,8 @@ import { useState } from 'react';
 import {postPatentId } from '../../utilities/api/patentData';
 
 export default function PatentSearchForm() {
-    const formReg = /([A-Z|a-z]{2}(\d+)[A-Z|a-z]{1,})/g
-    const [formData, setFormData] = useState('')
+    // const formReg = /([A-Z|a-z]{2}(\d+)[A-Z|a-z]{1,})/g
+    const [formData, setFormData] = useState({})
     const handleChange = (evt) => {
         const newFormData = {
             ...formData, // use the existing formData
@@ -17,28 +17,38 @@ export default function PatentSearchForm() {
 
     const handleSubmit = async (evt) => {
         evt.preventDefault()
-        const cleanedFormData = {...formData, patentId: formData.patentId.replaceAll(' ', '')}
-        try {
-            const patentData = await postPatentId(cleanedFormData)
-            // console.log(patentData)
-        } catch (err) {
-            console.log(err)
-            // await updateDocument({classification: null})
-        }
+        // const cleanedFormData = {...formData, patentId: formData.patentId.replaceAll(' ', '')}
+        // try {
+        //     const patentData = await postPatentId(cleanedFormData)
+        //     // console.log(patentData)
+        // } catch (err) {
+        //     console.log(err)
+        //     // await updateDocument({classification: null})
+        // }
+        console.log(formData)
     }
-    const disabled = !formReg.test(formData.patentId)
+    const disabled = !formData.patentIdEnd || !formData.patentIdStart || (parseInt(formData.patentIdStart) >= parseInt(formData.patentIdEnd))
     
     return(
-        <form className="PatentSearchForm" onSubmit={handleSubmit}>
+        <div className="PatentSearchForm" onSubmit={handleSubmit}>
             <div className='searchForm'>
-                <label htmlFor="patentId">
-                    <svg width="15" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5.14878 5.15857L16.4014 16.4112M16.4014 5.15857L5.14878 16.4112M19.5325 13.28L13.28 19.5325C11.9101 20.9024 9.65961 20.9024 8.27994 19.5325L2.02741 13.28C0.65753 11.9101 0.65753 9.65961 2.02741 8.27994L8.27994 2.02741C9.64982 0.65753 11.9003 0.65753 13.28 2.02741L19.5325 8.27994C20.9024 9.65961 20.9024 11.9101 19.5325 13.28Z" stroke="#1B2B41" strokeOpacity="0.6" strokeWidth="1.46773" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                </label>
-                <input onChange={handleChange} type="text" name='patentId' id='patentId' className='form-control me-2' placeholder='Enter Patent Number / URL'/>
-                <button type="submit" className="" disabled={disabled}>Submit</button>
+                <form id='patentForm' className='patentForm'>
+                    <div className='country'>
+                        US:
+                    </div>
+                    <div className='startRange'>
+                        <label htmlFor='patentIdStart'>Enter Range Start</label>
+                        <input onChange={handleChange} type="number" name='patentIdStart' id='patentIdStart' className='form-control' placeholder='Start'/>
+                    </div>
+                    <div className='separator'>-</div>
+                    <div className='startRange'>
+                        <label htmlFor='patentIdEnd'>Enter Range End</label>
+                        <input onChange={handleChange} type="number" name='patentIdEnd' id='patentIdEnd' className='form-control' placeholder='End'/>
+                    </div>
+                </form>
+                {/* <input onChange={handleChange} type="text" name='patentId' id='patentId' className='form-control me-2' placeholder='Enter Patent Number / URL'/> */}
+                <button type="submit" form='patentForm' className="submitButton" disabled={disabled}>Submit</button>
             </div>
-        </form>
+        </div>
     )
 }
